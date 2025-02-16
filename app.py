ae23f5f4f6d7da4c194d47ec457e1312daf12458
topdev126 <que.quao126@gmail.com>
2025-02-16 11:06:07 -0800

    9999

--- app.py
+++ app.py
@@ -407,5 +407,5 @@ driver_account = start_driver("account")
 Trades_history = {}
 run_background_tasks()
 
-# if __name__ == "__main__":
-#     socketio.run(app, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000)
