7e2d4a5bb2994748900e7fbeed0293688427fb5f
topdev126 <que.quao126@gmail.com>
2025-02-16 10:36:29 -0800

    6666

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def start_driver(option):
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument('--profile-directory=Default')
 
     driver = webdriver.Chrome(options=chrome_options)
 
